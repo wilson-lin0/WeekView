53ea079ca3b0db9767d5471c4200bf00b22fc367
emilyjea <emilyyjea@gmail.com>
2023-06-19 13:28:35 -0400

    Commit Driver

--- src/main/java/cs3500/pa05/Driver.java
+++ src/main/java/cs3500/pa05/Driver.java
@@ -1,6 +1,19 @@
 package cs3500.pa05;
 
-public class Driver {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Driver extends Application {
+
+  @Override
+  public void start(Stage stage) {
+    try {
+      stage.show();
+    } catch (IllegalStateException exc) {
+      System.err.println("Unable to load GUI.");
+    }
+  }
   public static void main(String[] args) {
+    launch();
   }
 }
