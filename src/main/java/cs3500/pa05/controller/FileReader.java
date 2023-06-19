9f101256259176a63d63fc5e487b3d824aec8d3c
emilyjea <emilyyjea@gmail.com>
2023-06-19 13:24:48 -0400

    Commit FileReader and FileWriter

--- src/main/java/cs3500/pa05/controller/FileReader.java
+++ src/main/java/cs3500/pa05/controller/FileReader.java
@@ -1,4 +1,96 @@
 package cs3500.pa05.controller;
 
-public class FileReader {
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+
+/**
+ * Filereader class that implements FileVisitor
+ * Reads the files and determines what to do for each file/dir
+ */
+public class FileReader implements FileVisitor<Path> {
+
+  private ArrayList<File> list;
+
+  FileReader(ArrayList<File> l) {
+    list = l;
+  }
+
+  /**
+   * Returns the list of paths
+   *
+   * @return ArrayList the list of paths to be returned
+   */
+  public ArrayList<File> getList() {
+    return list;
+  }
+
+  /**
+   * Called everytime the file system walker encounters a file
+   *
+   * @param file a reference to the file
+   * @param attr the file's basic attributes
+   * @return directive on how to process current item's siblings
+   */
+  @Override
+  public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
+
+    String fileName = file.getFileName().toString();
+    if (fileName.endsWith(".bujo")) {
+      list.add(file.toFile());
+    }
+
+    return CONTINUE;
+  }
+
+  /**
+   * What do do after processing all items in a directory
+   *
+   * @param dir a reference to the directory
+   * @param exec {@code null} if the iteration of the directory copletes without
+   *                         an error; otherwise the I/O exception that caused the
+   *                         iteration of the directory to complete prematurely
+   * @return in all cases, continue processing the sibling and child items of current item
+   */
+  @Override
+  public FileVisitResult postVisitDirectory(Path dir, IOException exec) {
+    System.out.format("Finishing Directory: %s%n", dir);
+    return CONTINUE;
+  }
+
+  /**
+   * What to do at the beginning of processing a directory
+   *
+   * @param dir a reference to the directory
+   * @param attrs the directory's basic attributes
+   * @return a directive to continue processing siblings and children of current item
+   */
+  @Override
+  public FileVisitResult preVisitDirectory(Path dir,
+                                           BasicFileAttributes attrs) {
+    System.out.format("Starting Directory: %s%n", dir);
+    return CONTINUE;
+  }
+
+  /**
+   * Called when a file cannot be visited for some undetermined reason
+   *
+   * @param file a reference to the file
+   * @param exc the I/O exception that prevented the file from being visited
+   * @return error
+   */
+  @Override
+  public FileVisitResult visitFileFailed(Path file, IOException exc) {
+    System.err.println(exc);
+    return CONTINUE;
+  }
 }
+
+
+
--- src/main/java/cs3500/pa05/controller/FileWriter.java
+++ src/main/java/cs3500/pa05/controller/FileWriter.java
@@ -1,4 +1,40 @@
 package cs3500.pa05.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FileEditor class to edit/write the final output file
+ */
 public class FileWriter {
+  private File file;
+  private Path path;
+
+  FileWriter(File file) {
+    this.file = file;
+    this.path = file.toPath();
+  }
+
+  /**
+   * Writes the file with only text enclosed in "[[" and "]]" and headers.
+   *
+   * @param listFiles the list of files to write and merge
+   * @return File the file to output the contents
+   * @throws IOException throws when file is not found
+   */
+  public File writeFile(ArrayList<File> listFiles) throws IOException {
+    for (File file : listFiles) {
+      List<String> lines = Files.readAllLines(file.toPath());
+      for (String text : lines) {
+        byte[] data = text.concat("\n").getBytes();
+        Files.write(this.path, data, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+      }
+    }
+    return this.file;
+  }
 }
