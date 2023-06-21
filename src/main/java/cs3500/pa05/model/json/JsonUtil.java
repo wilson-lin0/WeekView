b82a5562d0f2748ecfe8400f9c8a5afbac1a5e02
emilyjea <emilyyjea@gmail.com>
2023-06-21 16:33:36 -0400

    Changes by sarah

--- src/main/java/cs3500/pa05/model/json/JsonUtil.java
+++ src/main/java/cs3500/pa05/model/json/JsonUtil.java
@@ -13,65 +13,23 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
  */
 public class JsonUtil {
   /**
-   * Deserializes the given JSON string to a JsonNode.
-   *
-   * @param jsonString the JSON string to deserialize
-   * @return the deserialized JsonNode object
-   * @throws IOException if an error occurs during deserialization
-   * @throws JsonProcessingException if an error occurs during deserialization
-   */
-  public static JsonNode deserializeJson(String jsonString) throws JsonProcessingException {
-    ObjectMapper mapper = new ObjectMapper();
-    return mapper.readTree(jsonString);
-  }
-
-  /**
    * Converts a given record object to a JsonNode.
    *
    * @param record the record to convert
    * @return the JsonNode representation of the given record
    * @throws IllegalArgumentException if the record could not be converted correctly
    */
-  public static JsonNode serializeRecord(Record record) throws IllegalArgumentException {
+  public Week convertWeek(Week record) throws IllegalArgumentException {
     try {
       ObjectMapper mapper = new ObjectMapper();
-      return mapper.convertValue(record, JsonNode.class);
+      return mapper.convertValue(record, Week.class);
     } catch (IllegalArgumentException e) {
       throw new IllegalArgumentException("Given record cannot be serialized");
     }
   }
 
-  /**
-   * Deserializes a given JsonNode to a record object.
-   *
-   * @param jsonNode the JsonNode to deserialize
-   * @param recordClass the class of the record object
-   * @param <Record> the record object
-   * @return the deserialized record object
-   * @throws IllegalArgumentException if the JsonNode could not be deserialized correctly
-   */
-  public static <Record> Record deserializeRecord(JsonNode jsonNode, Class<Record> recordClass)
-      throws IllegalArgumentException {
-    try {
-      ObjectMapper mapper = new ObjectMapper();
-      Record record = mapper.treeToValue(jsonNode, recordClass);
-      return constructRecordObject(record, jsonNode, mapper);
-    } catch (JsonProcessingException e) {
-      throw new IllegalArgumentException("Given JsonNode cannot be deserialized to the record " +
-          "object");
-    }
-  }
+  public static
 
-  private static <Record> Record constructRecordObject(Record record, JsonNode jsonNode, ObjectMapper mapper)
-      throws JsonProcessingException {
-    JsonNode fieldsNode = mapper.valueToTree(record);
-    Iterator<String> fieldNames = fieldsNode.fieldNames();
-    while (fieldNames.hasNext()) {
-      String fieldName = fieldNames.next();
-      JsonNode fieldValue = jsonNode.get(fieldName);
-      ((ObjectNode) fieldsNode).replace(fieldName, fieldValue);
-    }
-    return mapper.treeToValue(fieldsNode, (Class<Record>) record.getClass());
-  }
+
 }
 
