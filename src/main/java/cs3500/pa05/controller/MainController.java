5ab9f0eaed205826ab6caf62a9c9301df174eb47
wilson-lin0 <wilsonlinwilin@gmail.com>
2023-06-21 21:35:20 -0400

    Open/New should work now

--- src/main/java/cs3500/pa05/controller/MainController.java
+++ src/main/java/cs3500/pa05/controller/MainController.java
@@ -1,16 +1,23 @@
 package cs3500.pa05.controller;
 
+import static cs3500.pa05.model.enumerations.Days.verifyDay;
+
 import cs3500.pa05.model.Event;
 import cs3500.pa05.model.Task;
 import cs3500.pa05.model.WeekView;
+import cs3500.pa05.model.enumerations.Days;
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -50,22 +57,54 @@ public class MainController extends AbstractController {
   private Label totalTasksLabel;
   @FXML
   private Label tasksCompletedLabel;
+  @FXML
+  private TextField createEventName;
+  @FXML
+  private TextField createEventDescription;
+  @FXML
+  private TextField createEventDay;
+  @FXML
+  private TextField createEventStartTime;
+  @FXML
+  private TextField createEventDuration;
 
   @FXML
-  protected HBox sundayBox;
+  private TextField createTaskName;
   @FXML
-  protected HBox mondayBox;
+  private TextField createTaskDescription;
   @FXML
-  protected HBox tuesdayBox;
+  private TextField createTaskDay;
   @FXML
-  protected HBox wednesdayBox;
+  private CheckBox taskCompleteCheck;
   @FXML
-  protected HBox thursdayBox;
+  private HBox sundayBox;
   @FXML
-  protected HBox fridayBox;
+  private HBox mondayBox;
   @FXML
-  protected HBox saturdayBox;
+  private HBox tuesdayBox;
+  @FXML
+  private HBox wednesdayBox;
+  @FXML
+  private HBox thursdayBox;
+  @FXML
+  private HBox fridayBox;
+  @FXML
+  private HBox saturdayBox;
   private final Popup startMenu;
+  private Popup eventCreationPopup;
+  private Popup taskCreationPopup;
+  @FXML
+  private Label warningEventLabel;
+  @FXML
+  private Label warningTaskLabel;
+  @FXML
+  private Button submitEventButton;
+  @FXML
+  private Button exitEventButton;
+  @FXML
+  private Button submitTaskButton;
+  @FXML
+  private Button exitTaskButton;
   String file;
 
   /**
@@ -99,7 +138,6 @@ public class MainController extends AbstractController {
     });
     this.addTaskButton.setOnAction((event -> {
       addTask();
-      showGraphics();
     }));
     this.eventTaskLimitButton.setOnAction(event -> {
       setLimit();
@@ -147,16 +185,240 @@ public class MainController extends AbstractController {
    * Adds an event.
    */
   private void addEvent() {
-    new AddEventController(this.weekView, this.stage).run();
-    showGraphics();
+    try {
+      this.eventCreationPopup = new Popup();
+      FXMLLoader eventLoader = new FXMLLoader(getClass().getClassLoader().getResource(
+          "AddEvent.fxml"));
+      eventLoader.setController(this);
+      Scene createEventScene = eventLoader.load();
+      eventCreationPopup.getContent().add(createEventScene.getRoot());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    this.eventCreationPopup.show(this.stage);
+
+    this.submitEventButton.setOnAction(event -> {
+      addEventToList();
+      showGraphics();
+    });
+
+    this.exitEventButton.setOnAction(event -> {
+      this.eventCreationPopup.hide();
+    });
+  }
+
+  private void addEventToList() {
+    String eventName = null;
+    String description = null;
+    Days day = null;
+    String startTime = null;
+    int duration = -1;
+
+    boolean canEventContinue = canEventContinue();
+
+    try {
+      eventName = createEventName.getText();
+      day = verifyDay(this.createEventDay.getText());
+      if (day.equals(Days.INVALID)) {
+        warningEventLabel.setText("You entered an invalid day");
+        canEventContinue = false;
+      }
+      if (correctTimeFormat(createEventStartTime.getText())) {
+        startTime = createEventStartTime.getText();
+      } else {
+        warningEventLabel.setText("The format for the starting time is incorrect");
+        canEventContinue = false;
+      }
+      if (isInteger(createEventDuration.getText())) {
+        duration = Integer.parseInt(createEventDuration.getText());
+      } else {
+        warningEventLabel.setText("The duration is not an integer");
+        canEventContinue = false;
+      }
+    } catch (NullPointerException n) {
+      warningEventLabel.setText("You left a required field empty!");
+      canEventContinue = false;
+    }
+
+    try {
+      description = createEventDescription.getText();
+    } catch (NullPointerException n) {
+      // it is okay to not have a description
+    }
+
+    if (canEventContinue) {
+      weekView.updateEvent(new Event(eventName, description, day, startTime, duration));
+      updateEventLabelList();
+      this.eventCreationPopup.hide();
+    }
+  }
+
+  /**
+   * Returns true if you can add an event.
+   *
+   * @return true if you can add an event
+   */
+  private boolean canEventContinue() {
+    if (this.weekView.hasMaximumEvents()) {
+      if (this.weekView.returnEventList().size() < this.weekView.returnMaxEvent()) {
+        return true;
+      } else {
+        warningEventLabel.setText("You have reached the maximum amount of events: " +
+            this.weekView.returnMaxEvent());
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+
+  /**
+   * Returns true if the format is correct.
+   *
+   * @param timeString the string
+   * @return true if the format is correct
+   */
+  private boolean correctTimeFormat(String timeString) {
+    SimpleDateFormat dateFormat = new SimpleDateFormat("k:mm");
+    dateFormat.setLenient(false);
+
+    try {
+      Date time = dateFormat.parse(timeString);
+      return timeString.equals(dateFormat.format(time));
+    } catch (ParseException e) {
+      return false;
+    }
+  }
+
+  /**
+   * Returns true if the string is an integer.
+   *
+   * @param string the string
+   * @return true if the string is an integer.
+   */
+  private boolean isInteger(String string) {
+    try {
+      Integer.parseInt(string);
+      return true;
+    } catch (NumberFormatException e) {
+      return false;
+    }
+  }
+
+  /**
+   * Updates the event label list.
+   */
+  private void updateEventLabelList() {
+    List<Event> events = this.weekView.returnEventList();
+    if (events.size() > 0) {
+      Event event = events.get(events.size() - 1);
+      Label label = new Label("Event: " + event.getName() + '\n' +
+          "Description: " + event.getDescription() + '\n' +
+          "Start Time: " + event.getStartTime() + '\n' +
+          "Duration: " + event.getDuration()
+      );
+      label.setFont(new Font(10));
+      labellists.addEventToList(label, event.getDayOfWeek());
+    }
   }
 
   /**
    * Adds a task.
    */
   private void addTask() {
-    new AddTaskController(this.weekView, this.stage).run();
+    this.taskCreationPopup = new Popup();
+    try {
+      FXMLLoader taskLoader = new FXMLLoader(getClass().getClassLoader().getResource(
+          "AddTask.fxml"));
+      taskLoader.setController(this);
+      Scene createTaskScene = taskLoader.load();
+      taskCreationPopup.getContent().add(createTaskScene.getRoot());
+      this.taskCreationPopup.show(this.stage);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    this.submitTaskButton.setOnAction(event -> {
+      addTaskToList();
+      showGraphics();
+    });
+
+    this.exitTaskButton.setOnAction(event -> {
+      this.taskCreationPopup.hide();
+    });
     showGraphics();
+  }
+
+  /**
+   * Adds a task.
+   */
+  private void addTaskToList() {
+    String taskName = null;
+    String description = null;
+    Days day = null;
+    boolean completed = false;
+    boolean canContinue = canTaskContinue();
+
+    try {
+      taskName = createTaskName.getText();
+      day = verifyDay(this.createTaskDay.getText());
+      if (day.equals(Days.INVALID)) {
+        warningTaskLabel.setText("You entered an invalid day");
+        canContinue = false;
+      }
+      completed = taskCompleteCheck.isSelected();
+    } catch (NullPointerException n) {
+      warningTaskLabel.setText("You left a required field empty!");
+      canContinue = false;
+    }
+
+    try {
+      description = createTaskDescription.getText();
+    } catch (NullPointerException n) {
+      // it is okay to not have a description
+    }
+
+    if (canContinue) {
+      weekView.updateTask(new Task(taskName, description, day, completed));
+      updateTaskLabelList();
+      this.taskCreationPopup.hide();
+    }
+  }
+
+  /**
+   * Returns true if you can add a task.
+   *
+   * @return true if you can add a task
+   */
+  private boolean canTaskContinue() {
+    if (this.weekView.hasMaximumTasks()) {
+      if (this.weekView.returnTaskList().size() < this.weekView.returnMaxTask()) {
+        return true;
+      } else {
+        warningTaskLabel.setText("You have reached the maximum amount of tasks: " +
+            this.weekView.returnMaxTask());
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+
+  /**
+   * Updates the task label list.
+   */
+  private void updateTaskLabelList() {
+    List<Task> tasks = this.weekView.returnTaskList();
+    if (tasks.size() > 0) {
+      Task task = tasks.get(tasks.size() - 1);
+      Label label = new Label("Event: " + task.getName() + '\n' +
+          "Description: " + task.getDescription() + '\n' +
+          "Completed? " + task.isCompleted()
+      );
+      label.setFont(new Font(10));
+      labellists.addTaskToList(label, task.getDayOfWeek());
+    }
   }
 
   /**
@@ -194,6 +456,7 @@ public class MainController extends AbstractController {
    */
   private void openFile() {
     this.weekView.clearAll();
+    this.labellists.clearAll();
     file = fileNameTextField.getText() + ".bujo";
     this.weekView.openFile(file);
     startMenu.hide();
