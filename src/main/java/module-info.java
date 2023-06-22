e3a60d9ddc88d0f01a56a9f178ae63f39773758e
berhe-sa <berhe.s@icloud.com>
2023-06-22 03:55:31 -0400

    links

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,19 @@
-/**
- * Represents PA-05.
- */
 module cs3500.pa05 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.fasterxml.jackson.annotation;
-    requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
-    requires org.controlsfx.controls;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires com.fasterxml.jackson.annotation;
+  requires com.fasterxml.jackson.core;
+  requires com.fasterxml.jackson.databind;
+  requires org.controlsfx.controls;
+
   requires java.desktop;
 
   opens cs3500.pa05 to javafx.fxml;
-    exports cs3500.pa05;
-    exports cs3500.pa05.controller;
-    exports cs3500.pa05.model;
-    exports cs3500.pa05.view;
-    exports cs3500.pa05.model.json;
-    exports cs3500.pa05.model.enumerations;
-    opens cs3500.pa05.controller to javafx.fxml;
+  exports cs3500.pa05;
+  exports cs3500.pa05.controller;
+  exports cs3500.pa05.model;
+  exports cs3500.pa05.view;
+  exports cs3500.pa05.model.json;
+  exports cs3500.pa05.model.enumerations;
+  opens cs3500.pa05.controller to javafx.fxml;
 }
